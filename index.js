5b79466064aa3c07ae892a38971b3ca4a11678f1
SafiaJotey <safiaahmed1786@gmail.com>
2022-10-03 19:33:45 +0600

    Setup fix

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+module.exports = app;
 const mongoose = require('mongoose');
 const dotnev = require('dotenv').config();
 const app = require('./app');
